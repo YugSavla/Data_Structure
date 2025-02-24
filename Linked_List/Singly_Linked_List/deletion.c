6095ad774ba9378ad930df6e63b316cb6a746575
Yug Savla <yugsavla1@gmail.com>
2025-02-24 19:37:52 +0530

    Deletion beginning

---
+++ Linked_List/Singly_Linked_List/deletion.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node* link; 
+}Node;
+
+void display(Node* head,Node* ptr){
+    ptr=head;
+    while (ptr!=NULL)
+    {
+        printf("%d->",ptr->data);
+        ptr=ptr->link;
+    }
+    
+}
+
+void delete_begin(){}
+
+void delete_end(){}
+
+void delete_after(){}
+
+void delete_before(){}
+
+int main(){
+    int n;
+    
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+
+    // If the user enters a non-positive number of nodes, exit the program
+    if (n <= 0) {
+        printf("Invalid number of nodes.\n");
+        return 1;
+    }
+
+    Node *head = NULL, *temp;
+    head=(Node*)malloc(sizeof(Node));
+    printf("Enter data for node 1: ");
+    scanf("%d", &head->data);
+    head->link = NULL; // The first node points to NULL initially
+
+    // Pointer to the current node
+    Node* ptr = head;
+
+    // Create and link the remaining nodes
+    for (int i = 2; i <= n; i++) {
+        printf("Enter data for node %d: ",i);
+        temp = (Node*) malloc(sizeof(Node));
+        scanf("%d",&temp->data);
+        
+        ptr->link=temp;
+        ptr=ptr->link;
+        temp->link=NULL;
+        
+    }
+}
