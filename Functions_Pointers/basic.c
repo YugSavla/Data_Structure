0a982b43e8cc5ab2f55099e3fa6033c8eda78d61
Yug Savla <yugsavla1@gmail.com>
2025-03-15 19:13:00 +0530

    Ok

--- Functions_Pointers/basic.c
+++ Functions_Pointers/basic.c
@@ -4,7 +4,16 @@ void greet(){
     return ;
 }
 int main(){
-    greet();
-    greet();
+    // greet();
+    // greet();
+    int x=5;
+    int* ptr;
+    ptr=&x;
+    printf("\n%d",*ptr);
+    *ptr=7;
+    printf("\n%d",*ptr);
+    printf("\n%p",&x);
+    printf("\n%p",ptr);
+    printf("\n%p",&ptr);
     return 0;
 }
--- Linked_List/Singly_Linked_List/deletion.c
+++ Linked_List/Singly_Linked_List/deletion.c
@@ -46,17 +46,39 @@ void delete_after(Node* head,Node* ptr){
     scanf("%d",&data);
     ptr=head;
     Node* temp;
+    Node* temp1;
+
     while(ptr->data!=data){
-        temp=ptr;
+        
         ptr=ptr->link;
+        temp=ptr->link;
+
     }
-    temp->link=ptr->link;
-    free(ptr);
+    temp1=temp->link;
+    ptr->link=temp1;
+
+    free(temp);
 
     
 }
 
-// void delete_before(){}
+void delete_before(Node* head,Node*ptr){
+    int data;
+    printf("\nEnter the data Before want to delete: ");
+    scanf("%d",&data);
+    ptr=head;
+    Node* temp;
+    Node* temp1;
+    while (ptr->data!=data)
+    {
+        temp1=temp;
+        temp=ptr;
+        ptr=ptr->link;
+    }
+    temp1->link=ptr;
+    free(temp);
+    
+}
 
 int main(){
     int n;
@@ -93,6 +115,7 @@ int main(){
     display(head,ptr);
     // head=delete_begin(head,ptr);
     // delete_end(head,ptr);
-    delete_after(head,ptr);
+    // delete_after(head,ptr);
+    delete_before(head,ptr);
     display(head,ptr);
 }
