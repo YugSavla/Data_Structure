e15ba797f59a8d95eef010d25d892e2b1a3ee61c
Yug Savla <yugsavla1@gmail.com>
2025-04-10 22:50:09 +0530

    infix

--- Stack/infix_postfix.c
+++ Stack/infix_postfix.c
@@ -1,80 +1,88 @@
-    #include<stdio.h>
-    #include<stdlib.h>
-    #include<string.h>
-    #define max 100
-    char stack[max];
-    char infix[max];
-    char postfix[max];
-    int top=-1;
-    int j=0;
-    int prec(char c) {
-        
-        if (c == '^')
-            return 3;
-        else if (c == '/' || c == '*')
-            return 2;
-        else if (c == '+' || c == '-')
-            return 1;
-        else
-            return -1;
-    }
-    void push(char symbol){
-        stack[++top]=symbol;
-    }
-    char pop(){
-        char c=stack[top];
-        stack[top]='\0';
-        top--;
-        return c;
-    }
-    void inToPost(){
-        char symbol,next;
-        
-        for (int i = 0; i < strlen(infix); i++)
-        {
-            symbol=infix[i];
-            if(symbol=='('){
-                push(symbol);    
-            }
-            else if(symbol==')'){
-                while (top!=-1 && stack[top]!='('){
-                    next=pop();
-                    
-                    postfix[j++]=next;
-                }
-                if (top != -1) {
-                    pop();  // Pop the '(' but don't add to result
-                }
-                //postfix[j++]=symbol
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define max 100
+char stack[max];
+char infix[max];
+char postfix[max];
+int top=-1;
+int j=0;
+int prec(char c) {
+    
+    if (c == '^')
+        return 3;
+    else if (c == '/' || c == '*')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+void push(char symbol){
+    stack[++top]=symbol;
+}
+char pop(){
+    char c=stack[top];
+    stack[top]='\0';
+    top--;
+    return c;
+}
+void inToPost(){
+    char symbol,next;
+    
+    for (int i = 0; i < strlen(infix); i++)
+    {
+        symbol=infix[i];
+        if(symbol=='('){
+            push(symbol);    
+        }
+        else if(symbol==')'){
+            while (top!=-1 && stack[top]!='('){
+                next=pop();
                 
+                postfix[j++]=next;
             }
-            else if(symbol=='+' || symbol=='-' || symbol=='*' || symbol=='/' || symbol=='^'){
-                while(top!=-1 && (prec(stack[top])>=prec(symbol))){
+            if (top != -1) {
+                pop();  // Pop the '(' but don't add to result
+            }
+            //postfix[j++]=symbol
+            
+        }
+        else if(symbol=='+' || symbol=='-' || symbol=='*' || symbol=='/' || symbol=='^'){
+            // Handle right associativity for '^' operator
+            if (symbol == '^') {
+                while(top!=-1 && (prec(stack[top]) > prec(symbol))){
                     next=pop();
                     postfix[j++]=next;    
                 }
-                push(symbol);
+            } else {
+                while(top!=-1 && (prec(stack[top]) >= prec(symbol))){
+                    next=pop();
+                    postfix[j++]=next;    
+                }
             }
-            else{
-                postfix[j++]=symbol;
-            }
+            push(symbol);
         }
-        while(top!=-1){
-            postfix[j++]=stack[top--];
+        else{
+            postfix[j++]=symbol;
         }
-        postfix[j]='\0';
-        
     }
-    int main() {
-        printf("Enter the infix: \n");
-        fgets(infix, sizeof(infix), stdin);  // Use fgets to avoid unsafe gets
-        infix[strcspn(infix, "\n")] = '\0';  // Remove the newline character from fgets input
-        inToPost();
-        
-        printf("checking stack");
-        puts(stack);
-        printf("Postfix: ");
-        puts(postfix);  // Print the postfix expression
+    while(top!=-1){
+        postfix[j++]=stack[top--];
+    }
+    postfix[j]='\0';
     
-        return 0;
-    }
+}
+int main() {
+    printf("Enter the infix: \n");
+    fgets(infix, sizeof(infix), stdin);  // Use fgets to avoid unsafe gets
+    infix[strcspn(infix, "\n")] = '\0';  // Remove the newline character from fgets input
+    inToPost();
+    
+    printf("checking stack: ");
+    puts(stack);
+    printf("Postfix: ");
+    puts(postfix);  // Print the postfix expression
+
+    return 0;
+}
